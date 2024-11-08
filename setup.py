54685063f24cb10efa3a0b0d3b312b15ad6c97df
rheyoampoyo <rheyoampoyo@gmail.com>
2024-11-07 19:08:38 -0500

    Adding hw 9

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/rheyoampoyo/m09-hw',
+    author='Rheyan Ampoyo',
+    author_email='rheyoampoyo@gmail.com',
+    license='MIT',
+    packages=['m09hw']
+)
+
